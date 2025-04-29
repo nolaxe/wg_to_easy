647b5a5c2c6bca8cf68914d1e71adf9b51759e4f
nolaxe <54135136+nolaxe@users.noreply.github.com>
2025-04-30 01:23:18 +0500

    Update wg_run.sh

--- wg_run.sh
+++ wg_run.sh
@@ -3,9 +3,15 @@ sudo apt update && sudo apt install -y docker.io docker-compose
 sudo systemctl enable --now docker
 
 # 2
+#!/bin/bash
+
+read -sp "Enter VPN password: " vpn_password
+echo
+
 docker run -d \
   --name=wg-easy \
-  --env-file .env \
+  -e WG_HOST=$(curl -s ifconfig.me) \
+  -e PASSWORD="$vpn_password" \
   -p 51820:51820/udp \
   -p 51821:51821/tcp \
   --cap-add=NET_ADMIN \

4a2ba62704e367ccfc98cde1ee65a0dc910730bc
nolaxe <54135136+nolaxe@users.noreply.github.com>
2025-05-01 00:30:29 +0500

    Update run_wg.sh

--- run_wg.sh
+++ run_wg.sh
@@ -56,20 +56,9 @@ docker ps -a | grep wg-easy
 docker exec wg-easy wg show
 
 ###########################
-echo -e "\nКонтейнер успешно запущен!\nWG_HOST=$(curl -s ifconfig.me)\nPASSWORD=$vpn_password\nWeb-интерфейс доступен по адресу: http://$(curl -s ifconfig.me):51821"
-echo "|"
-echo "✅ Контейнер wg-easy успешно запущен!"
-echo "🔗 Админка: http://$(curl -s ifconfig.me):51821"
-echo "|"
-echo "end"
 
-
-echo "\nКонтейнер успешно запущен!"
-echo "\nHOST=$(curl -s ifconfig.me)"
-echo "\nPASSWORD=$vpn_password"
-echo "\nWeb-интерфейс доступен по адресу: http://$(curl -s ifconfig.me):51821"
-echo "|"
-echo "✅ Контейнер wg-easy успешно запущен!"
-echo "🔗 Админка: http://$(curl -s ifconfig.me):51821"
+echo -e "✅ Контейнер wg-easy успешно запущен!HOST=$(curl -s ifconfig.me)"
+echo "    Web-интерфейс http://$(curl -s ifconfig.me):51821"
+echo "    PASSWORD=$vpn_password"
 echo "|"
 echo "end"

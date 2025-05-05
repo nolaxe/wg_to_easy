e362895011757db878eb9132adf4e54b0f31e338
nolaxe <54135136+nolaxe@users.noreply.github.com>
2025-05-05 22:48:54 +0500

    Update wr_run0.sh

--- wr_run0.sh
+++ wr_run0.sh
@@ -45,7 +45,7 @@ fi
 docker run -d \
   --name=wg-easy \
   -e WG_HOST=$(curl -s ifconfig.me || echo "YOUR_EXTERNAL_IP") \
-  -e PASSWORD_HASH="$hash" \
+  -e PASSWORD=123456 \
   -p 51820:51820/udp \
   -p 51821:51821/tcp \
   --cap-add=NET_ADMIN \

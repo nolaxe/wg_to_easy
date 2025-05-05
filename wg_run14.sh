c93dc60ccabc393ec2e9edca12a3cc9ecce92738
nolaxe <54135136+nolaxe@users.noreply.github.com>
2025-05-05 23:31:27 +0500

    Update wg_run14.sh

--- wg_run14.sh
+++ wg_run14.sh
@@ -1,13 +1,39 @@
- # 1
+#!/bin/bash
+clear
+
+# ------------------------------------------------------------------
+echo "Установка Docker & Docker Compose"
+echo -e "\n"
+
+# Проверяем, установлен ли docker
+if ! command -v docker &> /dev/null; then
+    echo "Docker не установлен. Начинаем установку..."
+    sudo apt update && sudo apt install -y docker.io docker-compose
+    sudo systemctl enable --now docker
+    echo "Docker успешно установлен и запущен."
+else
+    echo "Docker уже установлен."
+    docker --version
+fi
+# Проверяем, установлен ли docker-compose
+if ! command -v docker-compose &> /dev/null; then
+    echo "Docker Compose не установлен. Устанавливаем..."
+    sudo apt install -y docker-compose
+else
+    echo "Docker Compose уже установлен."
+    docker-compose --version
+fi
+
+# or
 # sudo apt update && sudo apt install -y docker.io docker-compose
 # sudo systemctl enable --now docker
 
-# 2
-#!/bin/bash
+# ------------------------------------------------------------------
+echo "Установка WG"
+echo -e "\n"
 
-read -sp "Enter GUI password: " password
+read -sp "Введите пароль для веб интерфейса: " password
 echo
-
 
 docker run -d \
   --name=wg-easy \
@@ -19,9 +45,17 @@ docker run -d \
   --sysctl="net.ipv4.ip_forward=1" \
   --restart unless-stopped \
   weejewel/wg-easy
+# ------------------------------------------------------------------
 
 # 3 status
 docker ps -a | grep wg-easy
-
 # 4 check 
 docker exec wg-easy wg show
+
+###########################
+echo -e "\n"
+echo -e "✅ Контейнер wg-easy успешно запущен"
+echo "   Web-интерфейс http://$(curl -s ifconfig.me):51821"
+echo "   Пароль: $password"
+echo "-"
+echo "eof..."

26f263ca6a15951ab06717907600736bee2d4bca
nolaxe <54135136+nolaxe@users.noreply.github.com>
2025-05-06 01:33:37 +0500

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -1,8 +1,9 @@
-read -p "Убрать плашку веб интерфейса -Доступно обновление- (docker cp app.js wg-easy:/app/www/js/app.js) ? (y/n) " choice
+read -p "bender.png ? (y/n)" choice
+# read -p "Убрать плашку веб интерфейса -Доступно обновление- (docker cp app.js wg-easy:/app/www/js/app.js) ? (y/n) " choice
 if [ "$choice" = "y" ] || [ "$choice" = "Y" ]; then
     # Загрузка файлов
     echo "Загружаю файлы..."    
-    curl -sSL https://raw.githubusercontent.com/nolaxe/wg_to_easy/main/app_no_upd.js -o app.js
+    # curl -sSL https://raw.githubusercontent.com/nolaxe/wg_to_easy/main/app_no_upd.js -o app.js
     curl -sSL https://raw.githubusercontent.com/nolaxe/wg_to_easy/main/bender.png -o bender.png
     
     echo "Копирую в контейнер..."

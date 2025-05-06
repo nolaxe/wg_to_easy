e51cc532f3fd255d4d5338992e8ce21c32d287b4
nolaxe <54135136+nolaxe@users.noreply.github.com>
2025-05-06 23:02:38 +0500

    Update and rename wg_run_e.sh to wg_run_fast.sh

---
+++ wg_run_fast.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+clear
+echo "wg_run_fast.sh"
+docker run -d \
+  --name=wg-easy \
+  -e WG_HOST=$(curl -s ifconfig.me) \
+  -e PASSWORD=$(curl -s ifconfig.me) \
+  -p 51820:51820/udp \
+  -p 51821:51821/tcp \
+  --cap-add=NET_ADMIN \
+  --sysctl="net.ipv4.ip_forward=1" \
+  --restart unless-stopped \
+  weejewel/wg-easy
+echo "   http://$(curl -s ifconfig.me):51821"
+echo "   Пароль: $password"
+curl -sSL https://raw.githubusercontent.com/nolaxe/wg_to_easy/main/addons/app_new.js -o app.js
+curl -sSL https://raw.githubusercontent.com/nolaxe/wg_to_easy/main/addons/bender.png -o logo.png
+docker cp app.js wg-easy:/app/www/js/app.js
+docker cp logo.png wg-easy:/app/www/img/logo.png
+rm -f app.js logo.png
+echo "✅ Готово!"

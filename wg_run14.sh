7ae2bf7b99c866d678e7d01e8f910ce7487b5c4a
nolaxe <54135136+nolaxe@users.noreply.github.com>
2025-05-06 22:50:44 +0500

    Update wg_run14.sh

--- wg_run14.sh
+++ wg_run14.sh
@@ -7,12 +7,15 @@ echo "# Установка wg-easy v14 #"
 sudo apt update
 
 # 1 Установка Docker (только если не установлен)
-if ! command -v docker &>/dev/null; then
-    sudo apt install -y docker.io docker-compose
+if ! command -v docker &> /dev/null; then
+    sudo apt update && sudo apt install -y docker.io docker-compose-plugin
+    # sudo apt update && sudo apt install -y docker.io docker-compose
     sudo systemctl enable --now docker
     sudo usermod -aG docker $USER
-    echo "⚠️ Перезапустите сессию (или выполните 'newgrp docker') и запустите скрипт снова."
-    exit 1
+    echo "Docker успешно установлен и запущен."
+else
+    echo "Docker уже установлен."
+    docker --version
 fi
 
 # 2 Установка wg-easy
@@ -29,7 +32,7 @@ hash=$(docker run --rm -i python:slim sh -c \
    { echo "❌ Ошибка генерации хэша"; exit 1; })
 
 # 2.3 Удаление старого контейнера (если есть)
-echo "Удаление старого контейнера (если есть)"
+echo "Удаление старого контейнера wg-easy (если существует)"
 docker rm -f wg-easy 2>/dev/null || true
 
 # Скачивание образа (с проверкой ошибок)
@@ -48,34 +51,7 @@ docker run -d \
   --cap-add=NET_ADMIN \
   --sysctl="net.ipv4.ip_forward=1" \
   --restart unless-stopped \
-  ghcr.io/wg-easy/wg-easy:14   # launch
-
-
-
-# 2.1 Запрос пароля VPN
-read -sp "2 Password: " password
-echo
-
-# 2.2 Генерация bcrypt-хэша
-echo "Генерация хэша пароля..."
-hash=$(docker run --rm -i python:slim sh -c \
-  "pip install --quiet --no-cache-dir --root-user-action=ignore bcrypt >/dev/null 2>&1 && \
-   python -c 'import bcrypt; print(bcrypt.hashpw(\"$password\".encode(\"utf-8\"), bcrypt.gensalt(rounds=8)).decode(\"utf-8\"))'" || \
-   { echo "❌ Ошибка генерации хэша"; exit 1; })
-
-
-
-docker run -d \
-  --name=wg-easy-2 \  # Уникальное имя
-  -e WG_HOST=$(curl -s ifconfig.me || echo "YOUR_EXTERNAL_IP") \
-  -e PASSWORD_HASH="$hash" \
-  -p 51822:51820/udp \  # Изменён внешний UDP-порт
-  -p 51823:51821/tcp \  # Изменён внешний TCP-порт
-  --cap-add=NET_ADMIN \
-  --sysctl="net.ipv4.ip_forward=1" \
-  --restart unless-stopped \
-  ghcr.io/wg-easy/wg-easy:14
-  
+  ghcr.io/wg-easy/wg-easy:14   # launch  
 
 # weejewel/wg-easy
 # ghcr.io/wg-easy/wg-easy:14 <

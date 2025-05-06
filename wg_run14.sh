94d652479739f3c7036246a65e52510a85488668
nolaxe <54135136+nolaxe@users.noreply.github.com>
2025-05-06 22:56:04 +0500

    Update wg_run14.sh

--- wg_run14.sh
+++ wg_run14.sh
@@ -2,9 +2,8 @@
 clear
 
 echo "# Установка wg-easy v14 #"
-
-# 0 Обновление пакетов (делаем ДО проверки Docker)
-sudo apt update
+# ------------------------------------------------------------------
+echo -e "\nУстановка Docker & Docker Compose"
 
 # 1 Установка Docker (только если не установлен)
 if ! command -v docker &> /dev/null; then
@@ -19,21 +18,22 @@ else
 fi
 
 # 2 Установка wg-easy
+echo -e "\nУстановка WG"
 
-# 2.1 Запрос пароля VPN
-read -sp "1 Password: " password
+# 2.1 Удаление старого контейнера (если есть)
+echo "Удаление старого контейнера wg-easy (если существует)"
+docker rm -f wg-easy 2>/dev/null || true
+
+# 2.2 Запрос пароля VPN
+read -sp "Password: " password
 echo
 
-# 2.2 Генерация bcrypt-хэша
+# 2.3 Генерация bcrypt-хэша
 echo "Генерация хэша пароля..."
 hash=$(docker run --rm -i python:slim sh -c \
   "pip install --quiet --no-cache-dir --root-user-action=ignore bcrypt >/dev/null 2>&1 && \
    python -c 'import bcrypt; print(bcrypt.hashpw(\"$password\".encode(\"utf-8\"), bcrypt.gensalt(rounds=8)).decode(\"utf-8\"))'" || \
    { echo "❌ Ошибка генерации хэша"; exit 1; })
-
-# 2.3 Удаление старого контейнера (если есть)
-echo "Удаление старого контейнера wg-easy (если существует)"
-docker rm -f wg-easy 2>/dev/null || true
 
 # Скачивание образа (с проверкой ошибок)
 # if ! docker pull ghcr.io/wg-easy/wg-easy:latest; then
@@ -41,7 +41,7 @@ docker rm -f wg-easy 2>/dev/null || true
 #    exit 1
 # fi
 
-# 2.4 Запуск контейнера
+# 2.4 Запуск WG
 docker run -d \
   --name=wg-easy \
   -e WG_HOST=$(curl -s ifconfig.me || echo "YOUR_EXTERNAL_IP") \
@@ -51,7 +51,7 @@ docker run -d \
   --cap-add=NET_ADMIN \
   --sysctl="net.ipv4.ip_forward=1" \
   --restart unless-stopped \
-  ghcr.io/wg-easy/wg-easy:14   # launch  
+  ghcr.io/wg-easy/wg-easy:14
 
 # weejewel/wg-easy
 # ghcr.io/wg-easy/wg-easy:14 <
@@ -65,7 +65,6 @@ docker exec wg-easy wg show
 
 ###########################
 echo -e "\n"
-echo -e "✅ Контейнер wg-easy успешно запущен!"
 echo "   Web-интерфейс http://$(curl -s ifconfig.me):51821"
 echo "   Пароль: $password"
 echo "   Хэш пароля: $hash"

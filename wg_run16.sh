6244223234dc3e768c72127ec35ce84942324dd3
nolaxe <54135136+nolaxe@users.noreply.github.com>
2025-05-06 12:49:20 +0500

    Update wg_run16.sh

--- wg_run16.sh
+++ wg_run16.sh
@@ -89,12 +89,14 @@ if [ "$choice" = "y" ] || [ "$choice" = "Y" ]; then
     # Загрузка файлов
     echo "Загружаю файлы..."    
     curl -sSL https://raw.githubusercontent.com/nolaxe/wg_to_easy/main/app_no_upd.js -o app.js
-    curl -sSL https://raw.githubusercontent.com/nolaxe/wg_to_easy/main/bender.png -o bender.png
+    curl -sSL https://raw.githubusercontent.com/nolaxe/wg_to_easy/main/bender.png -o logo.png
     
     echo "Копирую в контейнер..."
     docker cp app.js wg-easy:/app/www/js/app.js
-    docker cp bender.png wg-easy:/app/www/img/logo.png
-    # rm -f app.js bender.png
+    docker cp logo.png wg-easy:/app/www/img/logo.png
+    
+    rm -f app.js logo.png
+    
     echo "✅ Готово!"
 else
     echo "ℹ️ Отмена."

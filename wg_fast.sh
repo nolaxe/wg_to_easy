d60fc4a59778127c49d08c8a7e2c25e3832956d0
nolaxe <54135136+nolaxe@users.noreply.github.com>
2025-05-06 23:18:31 +0500

    Update wg_fast.sh

--- wg_fast.sh
+++ wg_fast.sh
@@ -15,8 +15,8 @@ echo "   http://$(curl -s ifconfig.me):51821"
 # echo "   Пароль: $password"
 curl -sSL https://raw.githubusercontent.com/nolaxe/wg_to_easy/main/addons/app_new.js -o app.js
 curl -sSL https://raw.githubusercontent.com/nolaxe/wg_to_easy/main/addons/bender.png -o logo.png
-docker cp app.js wg-easy:/app/www/js/app.js
-docker cp logo.png wg-easy:/app/www/img/logo.png
+docker cp app.js wg-easy:/app/www/js/app.js > /dev/null 2>&1
+docker cp logo.png wg-easy:/app/www/img/logo.png > /dev/null 2>&1
 rm -f app.js logo.png
 echo -e "\n✅ Готово!"
 echo -e "\n"

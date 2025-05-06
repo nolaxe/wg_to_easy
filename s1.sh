a683b6409ae3c48609f6d18eaba6ed5c1b907107
nolaxe <54135136+nolaxe@users.noreply.github.com>
2025-05-06 20:05:53 +0500

    Update s1.sh

--- s1.sh
+++ s1.sh
@@ -7,3 +7,8 @@ eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
 
 cat ~/.ssh/id_ed25519.pub | xclip
+
+# Перейдите в Settings → SSH and GPG keys → New SSH key.
+# Вставьте содержимое id_ed25519.pub в поле "Key".
+
+# ssh -T git@github.com

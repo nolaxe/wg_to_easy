91ed5910fc501c20a10b5306874f4d6b6c090c30
nolaxe <54135136+nolaxe@users.noreply.github.com>
2025-05-06 12:41:48 +0500

    Update wg_run.sh

--- wg_run.sh
+++ wg_run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
 clear
 
-echo "Установка Docker & wg-easy ..."
+echo "# Установка wg-easy v14# "
 
 # 0 Обновление пакетов (делаем ДО проверки Docker)
 sudo apt update
@@ -18,30 +17,28 @@ fi
 
 # 2 Установка wg-easy
 
-# Запрос пароля VPN
-# Генерация bcrypt-хэша (требуется установленный node.js или docker)
+# 2.1 Запрос пароля VPN
 read -sp "Password: " password
 echo
 
-# 3. Генерация bcrypt-хэша
+# 2.2 Генерация bcrypt-хэша
 echo "Генерация хэша пароля..."
 hash=$(docker run --rm -i python:slim sh -c \
   "pip install --quiet --no-cache-dir --root-user-action=ignore bcrypt >/dev/null 2>&1 && \
    python -c 'import bcrypt; print(bcrypt.hashpw(\"$password\".encode(\"utf-8\"), bcrypt.gensalt(rounds=8)).decode(\"utf-8\"))'" || \
    { echo "❌ Ошибка генерации хэша"; exit 1; })
 
-# Удаление старого контейнера (если есть)
+# 2.3 Удаление старого контейнера (если есть)
+echo "Удаление старого контейнера (если есть)"
 docker rm -f wg-easy 2>/dev/null || true
 
 # Скачивание образа (с проверкой ошибок)
-#  docker pull weejewel/wg-easy:latest >/dev/null 2>&1 || true && \
-#  docker pull ghcr.io/wg-easy/wg-easy:latest >/dev/null 2>&1 || true && \
-if ! docker pull ghcr.io/wg-easy/wg-easy:latest; then
-    echo "❌ Ошибка: не удалось скачать ghcr.io/wg-easy/wg-easy:latest"
-    exit 1
-fi
+# if ! docker pull ghcr.io/wg-easy/wg-easy:latest; then
+#    echo "❌ Ошибка: не удалось скачать ghcr.io/wg-easy/wg-easy:latest"
+#    exit 1
+# fi
 
-# Запуск контейнера
+# 2.4 Запуск контейнера
 docker run -d \
   --name=wg-easy \
   -e WG_HOST=$(curl -s ifconfig.me || echo "YOUR_EXTERNAL_IP") \
@@ -51,11 +48,11 @@ docker run -d \
   --cap-add=NET_ADMIN \
   --sysctl="net.ipv4.ip_forward=1" \
   --restart unless-stopped \
-  ghcr.io/wg-easy/wg-easy:latest   # launch
+  ghcr.io/wg-easy/wg-easy:14   # launch
 
 # weejewel/wg-easy
+# ghcr.io/wg-easy/wg-easy:14 <
 # ghcr.io/wg-easy/wg-easy:latest
-
 
 # 3 status
 docker ps -a | grep wg-easy

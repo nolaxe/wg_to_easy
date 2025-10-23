899bd5d348d241d37bb497706eb806e1a8a21ea4
nolaxe <54135136+nolaxe@users.noreply.github.com>
2025-10-23 23:21:03 +0500

    Update bender.sh

--- addons/bender.sh
+++ addons/bender.sh
@@ -19,8 +19,8 @@ read -p "need some bender.png ? (y/n)" choice
 if [ "$choice" = "y" ] || [ "$choice" = "Y" ]; then
     # Загрузка файлов
     echo "Загружаю файлы..."    
-    # curl -sSL https://raw.githubusercontent.com/nolaxe/wg_to_easy/main/app_no_upd.js -o app.js
-    curl -sSL https://raw.githubusercontent.com/nolaxe/wg_to_easy/main/bender.png -o bender.png
+    # curl -sSL https://raw.githubusercontent.com/nolaxe/wg_to_easy/main/addons/app_no_upd.js -o app.js
+    curl -sSL https://raw.githubusercontent.com/nolaxe/wg_to_easy/main/addons/bender.png -o bender.png
     
     echo "Копирую в контейнер..."
     # docker cp app.js wg-easy:/app/www/js/app.js
